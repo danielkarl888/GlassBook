433aae195e5a04e1b310dbade54625fb91402d69
danielkarl888 <danielkarl888@gmail.com>
2022-12-14 11:35:46 +0200

    age & country was added to register

--- src/RegisterPage.js
+++ src/RegisterPage.js
@@ -6,8 +6,11 @@ function RegisterPage() {
 
     const [newUser, setNewUser] = useState({
         user_name: "",
-        display: "",
+        user_id: "",
         password: "",
+        country: "",
+        age: "",
+        comments: ""
     });
     const handleuser_nameChange = (event) => {
         setNewUser({ ...newUser, user_name: event.target.value })
@@ -17,9 +20,13 @@ function RegisterPage() {
         setNewUser({ ...newUser, password: event.target.value })
         console.log(newUser.password);
     }
-    const handleDisplayNameChange = (event) => {
-        setNewUser({ ...newUser, display: event.target.value })
-        console.log(newUser.display);
+    const handleCountryChange = (event) => {
+        setNewUser({ ...newUser, country: event.target.value })
+        console.log(newUser.country);
+    }
+    const handleAgeChange = (event) => {
+        setNewUser({ ...newUser, age: event.target.value })
+        console.log(newUser.age);
     }
     const [submitted, setSubmitted] = useState(false);
     const [successMessage, setSuccessMessage] = useState(false);
@@ -27,40 +34,44 @@ function RegisterPage() {
         event.preventDefault();
         validAllandRegister(newUser);
         var bool = validAllandRegister(newUser);
-        setNewUser({user_name: "",
-        display: "",
-        password: "",
-        conversations: [{ user_name: "", messages: [{ src: "send", type: "text", context: "", time: "" }]}]});
+        setNewUser({
+            user_name: "",
+            user_id: "",
+            password: "",
+            country: "",
+            age: "",
+            comments: ""
+        });
         console.log(bool);
-        if(bool){
+        if (bool) {
             console.log("succedd!!");
             setSubmitted(true);
         } else {
             setSubmitted(false);
         }
     }
-    const validPassword = (newPassword)=>{
+    const validPassword = (newPassword) => {
         if (!(/\d/.test(newPassword) && /[a-zA-Z]/.test(newPassword))) {
             return false;
-        } else{
+        } else {
             return true;
         }
     }
-    const validuser_name = (uname)=>{
-        for(var i=0; i<userList.length; i++) {
-            if (uname===userList[i].user_name){
+    const validuser_name = (uname) => {
+        for (var i = 0; i < userList.length; i++) {
+            if (uname === userList[i].user_name) {
                 return false;
             }
         }
         return true;
     }
-    const validAllandRegister = (newUser)=>{
-        if (validPassword(newUser.password) && validuser_name(newUser.user_name)){
+    const validAllandRegister = (newUser) => {
+        if (validPassword(newUser.password) && validuser_name(newUser.user_name)) {
             userList.push(newUser);
             setSuccessMessage(true);
-            activeUser.conversations=newUser.conversations;
-            activeUser.user_name=newUser.user_name;
-            activeUser.display=newUser.display;
+            activeUser.conversations = newUser.conversations;
+            activeUser.user_name = newUser.user_name;
+            activeUser.display = newUser.display;
             console.log(activeUser);
             console.log(userList[userList.length - 2])
             console.log(userList[userList.length - 1])
@@ -68,73 +79,83 @@ function RegisterPage() {
         }
         return false;
     }
-   
+
     return (
         <>
-        <form
-        autoComplete="off"
-        onSubmit={e => handleSubmit(e, newUser)}
-        className="col card mt-2"
-        id="conncectCard">
-        <div className="text-center">
-         <h1 className="display-3">Register</h1>
-         </div>
+            <form
+                autoComplete="off"
+                onSubmit={e => handleSubmit(e, newUser)}
+                className="col card mt-2"
+                id="conncectCard">
+                <div className="text-center">
+                    <h1 className="display-3">Register</h1>
+                </div>
 
 
-            <div className="form-floating mb-3 input-padding-5">
-                <input
-                    onChange={handleuser_nameChange}
-                    type="text"
-                    className="form-control mt-2"
-                    id="floatingUser"
-                    placeholder="user_name"
-                    name="user_name"
-                    value={newUser.user_name}>
-                </input>
-                {(!validuser_name(newUser.user_name) && !submitted && newUser.user_name!="") ? <div className=" m-1 badge rounded-pill bg-danger">please select a uniqe user name!</div> : null}
-                <label htmlFor="floatingUser" className="fs-4">user_name</label>
-            </div>
-            <>
                 <div className="form-floating mb-3 input-padding-5">
                     <input
-                        onChange={handlePasswordChange}
-                        type="password"
+                        onChange={handleuser_nameChange}
+                        type="text"
                         className="form-control mt-2"
-                        id="floatingPassword"
-                        placeholder="Password"
-                        name="Password"
-                        value={newUser.password}>
+                        id="floatingUser"
+                        placeholder="user_name"
+                        name="user_name"
+                        value={newUser.user_name}>
                     </input>
-                    {(!validPassword(newUser.password)  && !submitted && newUser.user_name != "") ? <div className="m-1 badge rounded-pill bg-danger">please select at least 1 letter and 1 number!</div> : null}
-                    <label htmlFor="floatingPassword" className="fs-4">Password</label>
+                    {(!validuser_name(newUser.user_name) && !submitted && newUser.user_name != "") ? <div className=" m-1 badge rounded-pill bg-danger">please select a uniqe user name!</div> : null}
+                    <label htmlFor="floatingUser" className="fs-4">User Name</label>
                 </div>
-            </>
-            <div className="form-floating mb-3 input-padding-5 ">
-                <input
-                onChange={handleDisplayNameChange}
-                type="text"
-                className="form-control mt-2"
-                id="floatingDisplay"
-                value={newUser.display}
-                 placeholder="Display Name"></input>
-                <label htmlFor="floatingDisplay" className="fs-4">Display Name</label>
-            </div>
+                <>
+                    <div className="form-floating mb-3 input-padding-5">
+                        <input
+                            onChange={handlePasswordChange}
+                            type="password"
+                            className="form-control mt-2"
+                            id="floatingPassword"
+                            placeholder="Password"
+                            name="Password"
+                            value={newUser.password}>
+                        </input>
+                        {(!validPassword(newUser.password) && !submitted && newUser.user_name != "") ? <div className="m-1 badge rounded-pill bg-danger">please select at least 1 letter and 1 number!</div> : null}
+                        <label htmlFor="floatingPassword" className="fs-4">Password</label>
+                    </div>
+                </>
+                <div className="form-floating mb-3 input-padding-5 ">
+                    <input
+                        onChange={handleCountryChange}
+                        type="text"
+                        className="form-control mt-2"
+                        id="floatingDisplay"
+                        value={newUser.country}
+                        placeholder="Country"></input>
+                    <label htmlFor="floatingDisplay" className="fs-4">Country</label>
+                </div>
+                <div className="form-floating mb-3 input-padding-5 ">
+                    <input
+                        onChange={handleAgeChange}
+                        type="text"
+                        className="form-control mt-2"
+                        id="floatingDisplay"
+                        value={newUser.age}
+                        placeholder="Age"></input>
+                    <label htmlFor="floatingDisplay" className="fs-4">Age</label>
+                </div>
 
-            <div className="form-floating mb-3 input-padding-5 p-3" id="register">
-                <div className="col-7" id="submit">
-                    <button  type="submit" className="btn btn-primary btn-karl offset-6 fs-4 mb-3" id="register-btn">
-                        <i className="bi bi bi-pen-fill"></i> Register</button>
+                <div className="form-floating mb-3 input-padding-5 p-3" id="register">
+                    <div className="col-7" id="submit">
+                        <button type="submit" className="btn btn-primary btn-karl offset-6 fs-4 mb-3" id="register-btn">
+                            <i className="bi bi bi-pen-fill"></i> Register</button>
+                    </div>
+                    <div className="col-7" id="submit">
+                        {(successMessage) ? <Link to='/chat'><button className="btn btn-success btn-karl offset-6 fs-4 mb-3" id="register-btn"><i className="bi bi-whatsapp"></i> Start Chatting!</button></Link> : null}
+                    </div>
+                    <div className="row">
+                        <div className='col-4'></div>
+                        <div className="col fs-5" id="submit" >Already Registered? <Link to='/' className="link" id="changeToLogin">Click here</Link> to Login</div>
+                        <div className='col-2'></div>
+                    </div>
                 </div>
-                <div className="col-7" id="submit">
-                {(successMessage) ? <Link to='/chat'><button  className="btn btn-success btn-karl offset-6 fs-4 mb-3" id="register-btn"><i className="bi bi-whatsapp"></i> Start Chatting!</button></Link> : null}
-                </div>
-                <div className="row">
-                <div className='col-4'></div>
-                <div className="col fs-5" id="submit" >Already Registered? <Link to='/' className="link" id="changeToLogin">Click here</Link> to Login</div>
-                <div className='col-2'></div>
-                </div>
-            </div>
-        </form>
+            </form>
 
         </>
     );
