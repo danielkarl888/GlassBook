68f4f77cee6f99df2d4561138fda62281ce83975
danielkarl888 <danielkarl888@gmail.com>
2022-12-20 16:09:20 +0200

    added fetch for main screen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import FormStartScreen from './FormStartScreen';
 function App() {
-      return (
-      <div className="container-fluid">
+
+  return (
+    <div className="container-fluid">
       <div className="row">
         <div className="col-1">
         </div>
-        <FormStartScreen/>
+        <FormStartScreen />
         <div className="col-1">
         </div>
       </div>
     </div>
-    );
+  );
 }
 export default App;
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,17 +1,43 @@
 import { Link } from "react-router-dom";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import userList from './ManagingUsersList/userList';
 import LinkToChat from "./LinkToChat";
 import activeUser from "./ManagingUsersList/activeUser"
 
 function MainPage() {
+    const [comments, setComments] = useState(null);
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('http://localhost:5182/api/Comments');
+            const json = await response.json();
+            setComments(json);
+        }
+        fetchData();
+    }, []);
+
+
     return (
         <div>
-            {activeUser.comments.map(comment => (
-                <div>
-                    <p>{comment.comment_txt} and the rate is {comment.rate}</p>
-                </div>
-            ))}
+            <table>
+                <tr>
+                    <th>book_id</th>
+                    <th>user_name</th>
+                    <th>rate</th>
+                    <th>comment_txt</th>
+                </tr>
+
+                {comments ? (
+
+                    comments.map(comment => <tr>
+                        <td>{comment.book_id}</td>
+                        <td>{comment.user_name}</td>
+                        <td>{comment.rate}</td>
+                        <td>{comment.comment_txt}</td>
+                    </tr>)
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </table>
         </div>
     )
 
