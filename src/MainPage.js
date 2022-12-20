cc3a265c4c3312d28be2ff79f5327a82e80a37d6
danielkarl888 <danielkarl888@gmail.com>
2022-12-20 17:46:29 +0200

    style and date fixed

--- src/MainPage.js
+++ src/MainPage.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import userList from './ManagingUsersList/userList';
 import LinkToChat from "./LinkToChat";
 import activeUser from "./ManagingUsersList/activeUser"
+import moment from "moment";
 
 function MainPage() {
     const [comments, setComments] = useState(null);
@@ -15,29 +16,46 @@ function MainPage() {
         fetchData();
     }, []);
 
-
     return (
-        <div>
-            <table>
-                <tr>
-                    <th>book_id</th>
-                    <th>user_name</th>
-                    <th>rate</th>
-                    <th>comment_txt</th>
-                </tr>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    One of three columns
+                </div>
+                <div class="col-6">
+                    <div>
+                        <h2> Last 50 comments:</h2>
+                        <table className="table table-bordered">
+                            <tr>
+                                <th>#</th>
+                                <th>Title</th>
+                                <th>Reviewer</th>
+                                <th>rate</th>
+                                <th>Comment Text</th>
+                                <th>Date</th>
+                            </tr>
 
-                {comments ? (
+                            {comments ? (
 
-                    comments.map(comment => <tr>
-                        <td>{comment.book_id}</td>
-                        <td>{comment.user_name}</td>
-                        <td>{comment.rate}</td>
-                        <td>{comment.comment_txt}</td>
-                    </tr>)
-                ) : (
-                    <div>Loading...</div>
-                )}
-            </table>
+                                comments.map(comment => <tr>
+                                    <td>{comment.seq}</td>
+                                    <td>{comment.book_name}</td>
+                                    <td>{comment.user_name}</td>
+                                    <td>{comment.rate}</td>
+                                    <td>{comment.comment_txt}</td>
+                                    <td>{moment(comment.date).utc().format('DD/MM/YYYY')}</td>
+
+                                </tr>)
+                            ) : (
+                                <div>Loading...</div>
+                            )}
+                        </table>
+                    </div>                </div>
+                <div class="col-sm">
+                    One of three columns
+                </div>
+            </div>
+
         </div>
     )
 
